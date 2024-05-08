58db341721744303e5796a1647f5e29917a62f1e
rusiruavb <rusiruavb98@gmail.com>
2024-05-08 15:01:00 +0530

    feat: add permission service

--- src/permission/permission.controller.ts
+++ src/permission/permission.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Get } from '@nestjs/common';
+import { PermissionService } from './permission.service';
+import { CreatePermissionDto } from './dto/create-permission.dto';
 
 @Controller('permission')
-export class PermissionController {}
+export class PermissionController {
+  constructor(private readonly permissionService: PermissionService) {}
+
+  @Post('create')
+  async createPermission(@Body() input: CreatePermissionDto) {
+    return this.permissionService.createPermission(input);
+  }
+
+  @Get()
+  async getPermissions() {
+    return this.permissionService.getPermissions();
+  }
+}
--- src/permission/permission.service.ts
+++ src/permission/permission.service.ts
@@ -1,4 +1,69 @@
-import { Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { DataSource } from 'typeorm';
+import { CreatePermissionDto } from './dto/create-permission.dto';
+import { Permission } from './permission.entity';
 
 @Injectable()
-export class PermissionService {}
+export class PermissionService {
+  constructor(private readonly dataSource: DataSource) {}
+
+  async createPermission(input: CreatePermissionDto): Promise<Permission> {
+    const queryRunner = this.dataSource.createQueryRunner();
+
+    queryRunner.connect();
+    queryRunner.startTransaction();
+
+    try {
+      const permission = queryRunner.manager.create(Permission, {
+        name: input.name,
+        description: input.description,
+      });
+
+      await queryRunner.manager.insert(Permission, permission);
+
+      await queryRunner.commitTransaction();
+
+      return permission;
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+
+      if (error instanceof NotFoundException) {
+        throw error;
+      }
+
+      if (error instanceof UnauthorizedException) {
+        throw error;
+      }
+
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR, {
+        cause: new Error(JSON.stringify(error)),
+      });
+    } finally {
+      await queryRunner.release();
+    }
+  }
+
+  async getPermissions() {
+    try {
+      return await this.dataSource.manager.find(Permission);
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw error;
+      }
+
+      if (error instanceof UnauthorizedException) {
+        throw error;
+      }
+
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR, {
+        cause: new Error(JSON.stringify(error)),
+      });
+    }
+  }
+}
